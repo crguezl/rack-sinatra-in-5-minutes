1ae779c13e93a78d623a3e97ae17eb9f979a099a
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-10-20 20:37:58 +0100

    put example

--- my_app/my_app.rb
+++ my_app/my_app.rb
@@ -1,9 +1,20 @@
 require "my_framework"
 
 get '/' do
-  "<h1>hello world!</h1>\n"
+  <<-"EOS"
+   <h1><a href='/name?you=juana'>hello world!</a></h1>
+
+   <form action="/process" method="post">
+     Your name: <input type="text" name="firstname" autofocus><br>
+     <input type="submit" value="Submit">
+   </form>
+  EOS
 end
 
 get '/name' do
-  "<h1>Hi #{params["you"]}!</h1>\n"
+  %Q{<h1><a href="/">Hi #{params["you"]}!</a></h1>\n}
+end
+
+put '/process' do
+  %Q{<a href="/">Welcome to little Sinatra dear #{params['firstname']}!</a>}
 end
